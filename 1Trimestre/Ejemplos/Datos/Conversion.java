89fc4c6883080226713ade34160df267a10da62c
Gabri <gabrodflo@us.es.com>
2019-10-10 18:44:23 +0200

    Base de ejemplos del primer trimestre

---
+++ 1Trimestre/Ejemplos/Datos/Conversion.java
@@ -0,0 +1,7 @@
+public class Conversion{
+	public static void main(String[] args){
+		String numeroTexto = "77e";
+		int numero = Integer.parseInt(numeroTexto);
+		System.out.println(numero+3);
+	}
+}
