fb3076ce4d186751f6c7cff8925d2d77ef600816
Ronald <runi2012@hotmail.com>
2019-10-11 18:16:08 +0200

    Ejercicio Clase.java

---
+++ 1Trimestre/Ejercicios/Clase.java
@@ -0,0 +1,20 @@
+public class Clase{
+public static void main(String [] args) {
+int a = 1;
+int b = 2;
+int edad = 10;
+System.out.println(""+a+b);
+System.out.println(""+(a+b));
+System.out.println(args[0]+" "+ args[1]);
+if(edad >=18){
+	if(edad >= 80){
+	System.out.println("no puede conducir");
+}else{
+	System.out.println(" puede conducir");
+}
+}else{
+	System.out.println("no puede comprar alcohol :(");
+
+		}
+	}
+}
