63cb1fd462c71961984175d39afceebb24ef4483
Espe <edjunper@gmail.com>
2019-10-21 17:43:46 +0200

    Ejemplo de condicional

---
+++ 1Trimestre/Ejemplos/Condicionales/Condicional.java
@@ -0,0 +1,14 @@
+public class Condicional{
+	public static void main(String[] args) {
+		// TODO: lo que queramos
+		/* Es un comentario	*/
+		String texto = "Texto";
+		int numero = 12;
+		boolean entra = true;
+		if (numero < 10 ) {
+			System.out.println(texto +"\t"+ numero);
+		}else{
+			System.out.println("No ha entrado");
+		}
+	}
+}
