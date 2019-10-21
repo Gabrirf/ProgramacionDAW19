1c262fcb3209ee85aff257e5b36ad9e67a5784b4
Jesus Romero <jerona93@gmail.com>
2019-10-21 23:27:05 +0200

    Ejercicio imprimir letras minusculas ASCII

---
+++ 1Trimestre/Ejercicios/ImpMinus.java
@@ -0,0 +1,11 @@
+public class ImpMinus		//En este ejercicio imprimiremos todos las letras minusculas de la tabla ASCII
+{
+	public static void main(String[] args) {
+
+		int cont=97;
+
+		for(char c='a';c<='z';c++){
+		System.out.println(" "+c+"-"+cont++);
+		}
+	}
+}
