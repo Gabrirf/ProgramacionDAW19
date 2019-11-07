8db6c882499264e55e664cdddcc841bbac1d0d16
GnomeGuy <guillermocr98@gmail.com>
2019-11-07 15:51:15 +0100

    Ejercicio Array

---
+++ 1Trimestre/Ejercicios/EjemploArray.java
@@ -0,0 +1,11 @@
+public class EjemploArray {
+
+    public static void main(String[] args) {
+    	int[] arrayEnteros = {0,1,2,3,4,5,6,7,8,9};
+    	String[] arrayTexto = {null, "null", "", "hola",};
+
+    	System.out.println(Arrays.toString(arrayEnteros));
+    	System.out.println(Arrays.toString(arrayTexto));
+    	System.out.println(texto);
+    }
+}
