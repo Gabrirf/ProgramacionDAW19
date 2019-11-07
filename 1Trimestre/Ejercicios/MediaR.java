f9ee4380edf20dcbed488e5894156891a2d2ba28
Gabri <gabriel.rodriguez@adaits.es>
2019-11-07 15:57:41 +0100

    Correcion del ejercicio MediaR

--- 1Trimestre/Ejercicios/MediaR.java
+++ 1Trimestre/Ejercicios/MediaR.java
@@ -3,29 +3,39 @@ import java.util.Arrays;
 
 public class MediaR{
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
-        int[] array = new int[10];
+        int tam = 10;
         double sum = 0;
         int ceros = 0;
         int positivos = 0;
         int negativos = 0;
 
-        for(int i = 0; i < 10; i++){
+        // Si no indico argumento
+        if(args.length > 0){
+            tam = Integer.parseInt(args[0]);
+        }else{
+            tam = 10;
+        }
+        int[] array = new int[tam];
+
+        // Rellenamos el array
+        for(int i = 0; i < array.length; i++){
             System.out.println("Introduzca un numero porfavor: ");
             array[i] = sc.nextInt();
-
+        }
+        // Calculamos
+        for(int i = 0; i < array.length; i++){
             sum = sum + array[i];
 
             if(array[i] == 0){
-                ceros ++;
+                ceros++;
             }else if(array[i] < 0){
-                negativos ++;
-            }else if(array[i] >= 0){
-                positivos ++;
+                negativos++;
+            }else{
+                positivos++;
             }
-
         }
+
         String array2 = Arrays.toString(array);             //tengo que guardar el metodo en alguna variable para poder enseñarlo.
         double m = sum/array.length;                           //  array.length; es la longitud que tenga el array.
         System.out.println("Sus numeros son: "+ array2);
@@ -37,7 +47,7 @@ public class MediaR{
         if(positivos != 0){
             System.out.println("Cantidad de numero positivos: " + positivos);
         }
-        if(ceros == 0){
+        if(ceros != 0){
             System.out.println("Cantidad de 0 es " + ceros);
         }
         sc.close();
