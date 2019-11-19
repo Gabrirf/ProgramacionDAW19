f83bc50a6f501d06cd914c4afdea5a1fdab3574b
mariobalrod <marioballestero.rdg@gmail.com>
2019-11-19 16:34:48 +0100

    modificacion

--- EjerAvanzados/AdivinarNumeroVidas.java
+++ EjerAvanzados/AdivinarNumeroVidas.java
@@ -4,44 +4,60 @@ import java.util.Random;
     public class AdivinarNumeroVidas{
         public static void main(String[] args) {
 
-            int nVidas = 10;
+            boolean seguir = true;
             
             Scanner sc = new Scanner(System.in);
 
             System.out.print("Introduce un numero limite: ");
             int n = sc.nextInt();
 
+            while(seguir==true){
 
-            int random = (int) (Math.random() * n) + 1;
+                int nVidas = 7;
 
-            System.out.println("Tienes 10 VIDAS!");
+                int random = (int) (Math.random() * n) + 1;
+
+                System.out.println("Tienes 7 VIDAS!");
 
 
-            System.out.println("Adivina el numero!");
+                System.out.println("Adivina el numero!");
 
-            System.out.print("\nIntroduzca un numero: ");
-            int i = sc.nextInt();
-            nVidas--;
-            while(i!=random && nVidas!=0){
-
-                if(i>random){
-                    System.out.println("Es menor!");
-                    System.out.println("Te quedan "+nVidas+" vidas");
-                }
-                if(i<random){
-                    System.out.println("Es mayor!");
-                    System.out.println("Te quedan "+nVidas+" vidas");
-                }
+                System.out.print("\nIntroduzca un numero: ");
+                int i = sc.nextInt();
                 nVidas--;
-                System.out.print("\nIntroduzca otro numero: ");
-                i = sc.nextInt();      
-                
-            }
+                while(i!=random && nVidas!=0){
 
-            if(nVidas==0){
-                System.out.println("Has Perdido!");
-            }else{
-                System.out.println("Lo has adivinado el numero era el "+random);
+                    if(i>random){
+                        System.out.println("Es menor!");
+                        System.out.println("Te quedan "+nVidas+" vidas");
+                    }
+                    if(i<random){
+                        System.out.println("Es mayor!");
+                        System.out.println("Te quedan "+nVidas+" vidas");
+                    }
+                    nVidas--;
+                    System.out.print("\nIntroduzca otro numero: ");
+                    i = sc.nextInt();      
+                    
+                }
+
+                if(nVidas==0){
+                    System.out.println("Has Perdido!");
+                }else{
+                    System.out.println("Lo has adivinado el numero era el "+random);
+                }
+
+                System.out.println("\n¿Quieres jugar otra vez S/n?");
+                char resp = sc.next().toLowerCase().charAt(0);
+                
+                if(resp=='n'){
+                    seguir=false;
+                }else{
+
+                    System.out.print("\nIntroduce un numero limite: ");
+                    n = sc.nextInt();
+
+                }
             }
 
             sc.close();
