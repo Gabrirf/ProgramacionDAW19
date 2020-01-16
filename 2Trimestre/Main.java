ba8e55fd674169da9fcadebb39a25706e5c55473
Gabri <gabriel.rodriguez@adaits.es>
2020-01-16 17:10:17 +0100

    Constructores y Getter/Setter

--- 2Trimestre/Main.java
+++ 2Trimestre/Main.java
@@ -3,26 +3,29 @@ import clases.*;
 public class Main{
 	public static void main(String[] args) {
 		Agenda agenda = new Agenda();
-		Contacto c1 = new Contacto();
-		Contacto c2 = new Contacto();
-		Contacto c3 = new Contacto();
-
-		c1.nombre = "Lolo";
+		Contacto c1 = new Contacto("Lolo");
+		Contacto c2 = new Contacto("Pepa", 123456789);
+		Contacto c3 = new Contacto("Joselito", "123456789");
+		c1.setNombre("Paco");
+		String nombre = c1.getNombre();
+		System.out.println(nombre);
+		/*c1.nombre = "Lolo";
 		c2.nombre = "Pepa";
 		c3.nombre = "Joselito";
-
+		
 		c1.telefono = "123456789";
 		c2.telefono = "987654321";
-		c3.telefono = "123123123";
+		c3.telefono = "123123123";*/
 
 		Contacto[] contactos = new Contacto[3];
 		contactos[0] = c1;
 		contactos[1] = c2;
 		contactos[2] = c3;
+		//Contacto[] contactos = {c1,c2,c3};
 
 		agenda.contactos = contactos;
 
-		System.out.println(agenda.contactos[2].nombre);
+		System.out.println(agenda.contactos[2].getNombre());
 
 		/*Persona p1 = new Persona();
 		Persona p2 = new Persona();
--- 2Trimestre/clases/Contacto.java
+++ 2Trimestre/clases/Contacto.java
@@ -1,9 +1,37 @@
 package clases;
 
 public class Contacto{
-	public String nombre;
-	public String telefono;
+	String nombre;
+	String telefono;
+
+	public Contacto(){
+		this("Desconocido", null);
+	}
+	public Contacto(String nombre){
+		this(nombre, null);
+	}
+	public Contacto(String nombre, String telefono){
+		this.nombre = nombre;
+		this.telefono = telefono;
+	}
+	public Contacto(String nombre, int telefono){
+		this(nombre, String.valueOf(telefono));
+	}
+
+	public String getNombre(){
+		return this.nombre;
+	}
+	public String getTelefono(){
+		return this.telefono;
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public void setTelefono(String telefono){
+		this.telefono = telefono;
+	}
 
 	public void llamar(){}
 	public void colgar(){}
-}
+}     
