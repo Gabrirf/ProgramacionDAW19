5ed4d3fe7739d2a12d9583bb55ce55df3549f32b
Gabriel <grodriguez@emergya.com>
2020-05-11 18:00:20 +0200

    Repaso de funciones

---
+++ 1Trimestre/Ejemplos/7.Funciones/lib/Funciones2.java
@@ -0,0 +1,13 @@
+package lib;
+
+public class Funciones2{
+
+	public static void mostrar(){
+		System.out.println("Hola externo");
+	}
+
+    public void mostrarNoStatic(){
+		System.out.println("Hola No static");
+	}
+
+}
